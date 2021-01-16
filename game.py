b28a1760abae153b78e917c8cea10bf548ad2494
Omer <omerfkhan98@gmail.com>
2021-01-16 20:45:45 +0000

    add pipelines

--- game.py
+++ game.py
@@ -1,17 +1,7 @@
-import sys, pygame
+import sys, pygame, random
+from pipe import Pipe
 
 pygame.init()
-
-SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 500
-size = SCREEN_WIDTH, SCREEN_HEIGHT
-
-# game parameters
-gap = 50
-starting_point_ball = SCREEN_WIDTH/4, SCREEN_HEIGHT/2
-gravity = 0.001
-
-screen = pygame.display.set_mode(size)
 
     # o- - - - > x
     # '
@@ -20,35 +10,68 @@ screen = pygame.display.set_mode(size)
     # '
     # y
 
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+SIZE = SCREEN_WIDTH, SCREEN_HEIGHT
+
+# game parameters
+GAP = 100
+PIPE_WIDTH = 50
+DISTANCE_BETWEEN_PIPES = 300
+starting_point_ball = SCREEN_WIDTH/4, SCREEN_HEIGHT/2
+GRAVITY = 0.02
+PIPELINE = []
+PIPELINE_SIZE = 3
+
+screen = pygame.display.set_mode(SIZE)
+
 # ball parameters
 black = 0, 0, 0
 ball_x, ball_y = starting_point_ball
 ball_speed = 0
 
-# lower pipe parameters
-lower_pipe_width = 50
-lower_pipe_length = SCREEN_HEIGHT/2
-lower_pipe_x, lower_pipe_y = SCREEN_WIDTH, SCREEN_HEIGHT - lower_pipe_length
 
-# upper pipe parameters
-upper_pipe_width = 50
-upper_pipe_length = SCREEN_HEIGHT/2 - gap
-upper_pipe_x, upper_pipe_y = SCREEN_WIDTH, 0 
+def randomPipeHeight(): 
+    return SCREEN_HEIGHT - random.randint((SCREEN_HEIGHT - GAP)//4, (SCREEN_HEIGHT - GAP)//2)
+
+def generateInitialPipeline(pipeline, pipeline_size):
+    pipe_x, pipe_y = SCREEN_WIDTH, SCREEN_HEIGHT
+    for i in range(pipeline_size):
+        pipe_height = randomPipeHeight()
+        pipe_y = SCREEN_HEIGHT - pipe_height
+        pipeline.append(Pipe(pipe_x, pipe_y, pipe_height, PIPE_WIDTH))
+        pipe_x += DISTANCE_BETWEEN_PIPES  
+
+def drawPipes(pipeline):
+    for pipe in pipeline:
+        # lower pipeline
+        pygame.draw.rect(screen, (255,0,0), (pipe.x, pipe.y, pipe.width, pipe.height))
+        # upper pipeline 
+        lower_pipe_height = SCREEN_HEIGHT - pipe.height - GAP
+        pygame.draw.rect(screen, (255,0,0), (pipe.x, 0, pipe.width, lower_pipe_height))
+
+def updatePipeline(pipeline):
+    for pipe in pipeline:
+        print(pipe.__dict__)
+        pipe.x -= 1
+
+
+def drawBall():
+    pygame.draw.circle(screen, (255,0,0), (ball_x, ball_y), 5)
+
+generateInitialPipeline(PIPELINE, PIPELINE_SIZE)
+
+print(randomPipeHeight())
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE: ball_speed = -0.5
+            if event.key == pygame.K_SPACE: ball_speed = -1.5
 
-    pygame.draw.circle(screen, (255,0,0), (ball_x, ball_y), 5)
-    pygame.draw.rect(screen, (255,0,0), (lower_pipe_x, lower_pipe_y, lower_pipe_width, lower_pipe_length))
-    pygame.draw.rect(screen, (255,0,0), (upper_pipe_x, upper_pipe_y, upper_pipe_width, upper_pipe_length))
-
+    drawPipes(PIPELINE)
     pygame.display.flip()
     screen.fill(black)
-
     ball_y += ball_speed
-    ball_speed += gravity
-    lower_pipe_x -= 0.21
-    upper_pipe_x -= 0.21
+    ball_speed += GRAVITY
+    updatePipeline(PIPELINE)
--- pipe.py
+++ pipe.py
@@ -1,5 +1,5 @@
-class Pipe
-    def __init__ (self, x, y, length, width):
+class Pipe:
+    def __init__ (self, x, y, height, width):
         self.x = x
         self.y = y
         self.height = height 
@@ -8,9 +8,9 @@ class Pipe
     def update(increment):
         self.x += increment
 
-    def doesCollide?(x,y):
-        if (x > self.x and x < self.x + self.width) and 
-           (y > self.y and y < self.y + self.height):
+    def doesCollide(x,y):
+        if ((x > self.x and x < self.x + self.width) and 
+           (y > self.y and y < self.y + self.height)):
                return True
 
         return False
