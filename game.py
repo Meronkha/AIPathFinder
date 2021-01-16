70897ba02f372987526df2ff52919f03566167df
Omer <omerfkhan98@gmail.com>
2021-01-16 23:01:09 +0000

    clean up logic

--- game.py
+++ game.py
@@ -2,7 +2,6 @@ import sys, pygame, random
 from pipe import Pipe
 from ball import Ball
 
-
 FPS = 60 
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 500
@@ -13,10 +12,11 @@ DISTANCE_BETWEEN_PIPES = 300
 PERSISTENT_X = SCREEN_WIDTH/4
 PIPELINE = []
 PIPELINE_SIZE = 3
-GRAVITY = 0.1
-ROLL_RATE = 3
-BOUNCE = 2.5
+GRAVITY = 0.18
+ROLL_RATE = 2
+BOUNCE = 5
 USER_SCORE = 0 
+WHITE = (255, 255, 255)
 
 fps_clock = pygame.time.Clock()
 pygame.init()
@@ -41,36 +41,35 @@ def generateInitialPipeline(pipeline, pipeline_size):
 
 def drawPipes(pipeline):
     for pipe in pipeline:
-        # lower pipe
-        pygame.draw.rect(screen, (255,0,0), (pipe.x, pipe.y, pipe.width, pipe.height))
-        # upper pipe
+        pygame.draw.rect(screen, WHITE, (pipe.x, pipe.y, pipe.width, pipe.height))
         lower_pipe_height = SCREEN_HEIGHT - pipe.height - GAP
-        pygame.draw.rect(screen, (255,0,0), (pipe.x, 0, pipe.width, lower_pipe_height))
+        pygame.draw.rect(screen, WHITE, (pipe.x, 0, pipe.width, lower_pipe_height))
 
 def updatePipeline(pipeline):
     new_pipe = generatePipe(pipeline[-1], pipeline)
     for pipe in pipeline:
         pipe.x -= ROLL_RATE
 
-    if pipeline[0].x < 0:
+    if pipeline[0].x < -1*PIPE_WIDTH:
         pipeline.append(new_pipe)
         pipeline.pop(0)
         global USER_SCORE
         USER_SCORE += 1
         print(USER_SCORE)
-    return
 
 def drawBall(ball):
-    pygame.draw.circle(screen, (255,0,0), (ball.x, ball.y), 5)
+    pygame.draw.circle(screen, WHITE, (ball.x, ball.y), 5)
 
 def checkCollision(pipeline, ball):
     for pipe in pipeline:
         if pipe.doesCollide(ball.x, ball.y, GAP):
             return True
+    if (ball.y < 0 or ball.y > SCREEN_HEIGHT):
+        return True
+
     return False
 
 generateInitialPipeline(PIPELINE, PIPELINE_SIZE)
-
 ball = Ball(SCREEN_WIDTH/5, SCREEN_HEIGHT/2, 0,  GRAVITY)
 
 while True:
@@ -81,9 +80,12 @@ while True:
 
     ball.update()
     updatePipeline(PIPELINE)
+    
     if checkCollision(PIPELINE, ball): break
+
     drawBall(ball)
     drawPipes(PIPELINE)
+
     pygame.display.flip()
     screen.fill((0, 0, 0))
     fps_clock.tick(FPS)
